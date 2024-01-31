22c57ec0ef6e06ed10a778a9ee8ea036368ec805
Omer Katz <omer.katz@post.runi.ac.il>
2024-01-31 15:35:13 +0200

    Omer's HomeWork 7

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -15,16 +15,24 @@ public class HashTagTokenizer {
 		In in = new In(fileName);
 
 		// Your code here
-
+		for(int i = 0; i < 3000; i++){
+			dictionary[i] = in.readLine();
+		}
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
 		// Your code here
+		boolean exist = false;
+		for(int i = 0; i < 3000; i ++){
+			if(word.equals(dictionary[i]))
+				exist = true;
+		}
+		return exist;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
-
+		hashtag = hashtag.toLowerCase();
 		// Base case: do nothing (return) if hashtag is an empty string.
         if (hashtag.isEmpty()) {
             return;
@@ -33,8 +41,13 @@ public class HashTagTokenizer {
         int N = hashtag.length();
 
         for (int i = 1; i <= N; i++) {
-		
+				if(existInDictionary(hashtag.substring(0,i), dictionary) == true) {
+					System.out.println(hashtag.substring(0, i));
+					breakHashTag(hashtag.substring(i,N), dictionary);
+					break;
+				}
+			}
         }
     }
 
-}
+
